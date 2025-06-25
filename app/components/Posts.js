61af0e896fa44331ed414026b016009167a8abdd
fguedez1311 <46085315+fguedez1311@users.noreply.github.com.>
2025-06-25 14:20:32 -0400

    Se agegaron mejoras al router

--- app/components/Posts.js
+++
@@ -1,6 +0,0 @@
-export function Posts() {
-    const posts=document.createElement('section')
-    posts.id='posts'
-    posts.className='grid-fluid'
-    return posts
-}
--- app/components/Router.js
+++ app/components/Router.js
@@ -2,23 +2,23 @@ import api from "../helpers/wp_api.js";
 import { ajax } from "../helpers/ajax.js";
 import { PostCard } from "./PostCard.js";
 
-export function Router() {
+export async function Router() {
     const d = document,
     w= window,
-    posts= d.getElementById("posts");
+    $main= d.getElementById("main");
     let {hash} = w.location;
     console.log(hash);
-    posts.innerHTML=null
+    $main.innerHTML=null
     if(!hash || hash === "#/home"){
-        ajax({
+        await ajax({
                
                 url:api.POSTS,
                 cbSuccess:(posts)=>{
-                    //console.log(posts)
+                    //console.log($main)
                     let html=""
                     posts.forEach(post =>html+= PostCard(post))
-                    d.querySelector(".loader").style.display="none";
-                    d.getElementById("posts").innerHTML=html;
+                    $main.innerHTML=html;
+                   
                     
                       
                 }
@@ -26,17 +26,17 @@ export function Router() {
           })
     }
     else if(hash.includes("#/search")){
-       posts.innerHTML="<h2>Sección de Búsqueda</h2>"
+       $main.innerHTML="<h2>Sección de Búsqueda</h2>"
 
     }
     else if(hash.includes("#/contacto")){
-       posts.innerHTML="<h2>Sección de Contacto</h2>"
+       $main.innerHTML="<h2>Sección de Contacto</h2>"
     }
     else{
-       posts.innerHTML="<h2>Aquí cargará el  contenido del post seleccionado</h2>"
+       $main.innerHTML="<h2>Aquí cargará el  contenido del post seleccionado</h2>"
     }
     
-
+    d.querySelector(".loader").style.display="none";
     
 
 }
